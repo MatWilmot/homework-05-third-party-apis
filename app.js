cb787ee67184428536f888c22b5f8f2a644059e0
Mat Wilmot <matwilmot90@gmail.com>
2020-05-01 16:06:37 -0700

    textarea backgrounds now change color based on time relative to now

--- app.js
+++ app.js
@@ -2,24 +2,41 @@ $(document).ready(function () {
   var day = moment().format("dddd");
   var date = moment().format("MMM Do YYYY");
   var currentDay = day + ", " + date;
-  var currentHour = moment().format("h");
-  var displayHour = 0;
+  var currentHour = moment().format("H");
   var time = 0;
 
   $("#currentDay").append(currentDay);
 
   for (var i = 0; i < 12; i++) {
+    // Create time as an equation of i
     if (i < 6) {
       time = i + 7;
     } else {
       time = i - 5;
     }
-
+    // Set am/pm as an equation of i
     if (i < 5) {
       ampm = "am";
     } else {
       ampm = "pm";
     }
-    $(".container").append(`<h1>${time + " " + ampm}</h1>`);
+    // Append each line to the screen
+    $(".container").append(`<div class="row time-block">
+    <p class="text-center col-1 pt-2 border-top">${time + " " + ampm}</p>
+    <textarea name="TA${i}" id="TA${i}" cols="100" rows="3" placeholder=""></textarea>
+  </div>`);
+    // determine the background color of each row
+    if (i + 7 < currentHour) {
+      // make the background .past
+      $(`#TA${i}`).attr("class", "past");
+    }
+    if (i + 7 == currentHour) {
+      // make the background .present
+      $(`#TA${i}`).attr("class", "present");
+    }
+    if (i + 7 > currentHour) {
+      // make the background .future
+      $(`#TA${i}`).attr("class", "future");
+    }
   }
 });
